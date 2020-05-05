fd7567e50a4df483b9b449dd805dc5b7c29d0973
Wjshang <1437110107@qq.com>
2020-05-05 17:40:03 +0800

    1

---
+++ test/debug.py
@@ -0,0 +1,22 @@
+
+
+'''
+def outFun():   
+    def innerFun_0():#1.在内部定义一个函数
+        print("i am first innerFun")
+        return    
+    
+    def innerFun_1():#2.在内部定义另外一个函数
+        print("i am second innerFun")
+        return      
+    
+    #innerFun_0()   #3.使用innerFun_0
+    #innerFun_1()   #4.使用innerFun_2
+    
+    return
+
+outFun()
+
+'''
+
+a = []
